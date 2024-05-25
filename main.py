54e9d99b3a10fb2bc21895bf25ba63aa601b508d
CNath01 <92951005+CNath01@users.noreply.github.com>
2024-05-25 10:19:05 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import random
+nombre = "Camila"
+edad = 20
+
+nombre = input("Ingrese su nombre") #Ingresar cadenas
+edad2 = int(input("Ingrese su edad")) #Ingresar valores numericos
+x = "cdsds"
+contra = ""
+
+contra = contra + random.choice(x)
+print(contra)
